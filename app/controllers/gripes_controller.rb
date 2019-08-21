18dd9ca6ca86deb3c54637a5fdef821a533fb4fd
Art <artelo.studio@gmail.com>
2019-08-21 03:21:01 +0000

    added submit fields and form

--- app/controllers/gripes_controller.rb
+++ app/controllers/gripes_controller.rb
@@ -8,4 +8,15 @@ class GripesController < ApplicationController
     @gripe = Gripe.new
   end
 
+  def create
+    Gripe.create(place_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def place_params
+    params.require(:gripe).permit(:name, :beef, :fix)
+  end
+
 end
